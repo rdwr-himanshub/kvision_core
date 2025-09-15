5996504e9f376338669838cc439aa93a2a6faf92
Himanshu Batham <himanshub@radware.com>
2025-09-15 12:42:48 +0530

    commint two files..not commiting readme

--- somefile_sejal.java
+++ somefile_sejal.java
@@ -1,11 +1,9 @@
 class someClass
   {
-<<<<<<< HEAD
    bathma batham
-
-=======
-
-my code and more code
-    and more code
->>>>>>> cb67f73cbf1c8bd1bb45e9914c208133f0b6746b
+  bfgewabkigtf
+  ggt
+  ga
+  treqftreqtq
+  gaaga
   }
